4a46e6fb27728630d577798e67e844c2bcb87afa
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-29 19:53:44 +0530

    [Kumar Shubham]Added:Create a payroll service database and connect to program

---
+++ EmployeePayRollService/EmployeePayRollService/EmployeeRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayRollService
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = @"Data Source=DESKTOP-9FUO12F;Initial Catalog=Payroll_Service;Integrated Security=True";
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
