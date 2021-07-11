12e9b6bd912d145033e5cd3944630501e871b098
kmrshubham123 <kmrshubham123@gmail.com>
2021-07-11 20:35:43 +0530

    [Kumar Shubham]Added:Add Method Create and using ADO sync it with DataBase

--- EmployeePayRollService/EmployeePayRollService/EmployeeRepo.cs
+++ EmployeePayRollService/EmployeePayRollService/EmployeeRepo.cs
@@ -12,21 +12,23 @@ namespace EmployeePayRollService
     class EmployeeRepo
     {
         public static string connectionString = @"Data Source=DESKTOP-9FUO12F;Initial Catalog=Payroll_Service;Integrated Security=True";
-        SqlConnection sqlConnection = new SqlConnection(connectionString);
+      
         /// <summary>
         /// Method to ShowTable
         /// </summary>
         public void GetAllEmployee()
         {
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
             try //When an exception occurs, the control will move to the catch block
             {
+              
                 EmployeePayroll payroll = new EmployeePayroll();
-                using (this.sqlConnection)
+                using (sqlConnection)
                 {
                     string query = @"select * from EmployeePayroll";
                     //SqlCommand is interact with reational database or stored procedure to execute against SQL Server datdabase.
-                    SqlCommand cmd = new SqlCommand(query, this.sqlConnection);
-                    this.sqlConnection.Open();
+                    SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                    sqlConnection.Open();
                     //SqlDataReader Provides a way of reading a forward-only stream of rows from a SQL Server database.
                     SqlDataReader dr = cmd.ExecuteReader(); //Returns the sqlDataReader Object.
                     if (dr.HasRows) //Gets the value that indicates whether the SQLDataReader contains one or more rows.
@@ -56,7 +58,7 @@ namespace EmployeePayRollService
                     }
                     //close data reader
                     dr.Close();
-                    this.sqlConnection.Close();
+                    //this.sqlConnection.Close();
                 }
             }
             catch (Exception e) //Catch/Handle the exception  
@@ -65,7 +67,56 @@ namespace EmployeePayRollService
             }
             finally //this block of code will always get executed whether an exception occurs or not.
             {
-                this.sqlConnection.Close();
+                sqlConnection.Close();
+            }
+        }
+        /// <summary>
+        /// Create a add method 
+        /// using Store Procedure
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        public bool AddEmployee(EmployeePayroll employee)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            try
+            {
+              
+                using (sqlConnection)
+                {
+                    SqlCommand command = new SqlCommand("SpAddEmployeeDetail", sqlConnection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmployeeName", employee.EmployeeName);
+                    command.Parameters.AddWithValue("@Salary", employee.Salary);
+                    command.Parameters.AddWithValue("@StartDate", DateTime.Now);
+                    command.Parameters.AddWithValue("@Gender", employee.Gender);
+                    command.Parameters.AddWithValue("@PhoneNumber", employee.PhoneNumber);
+                    command.Parameters.AddWithValue("@Department", employee.Department);
+                    command.Parameters.AddWithValue("@Address", employee.Address);
+                    command.Parameters.AddWithValue("@BasicPay", employee.BasicPay);
+                    command.Parameters.AddWithValue("@Deduction", employee.Deduction);
+                    command.Parameters.AddWithValue("@TaxablePay", employee.TaxablePay);
+                    command.Parameters.AddWithValue("@IncomeTax", employee.IncomeTax);
+                    command.Parameters.AddWithValue("@NetPay", employee.NetPay);
+                    sqlConnection.Open();
+                    var result = command.ExecuteNonQuery();
+                    sqlConnection.Close();
+                    if (result != 0)
+                    {
+                        Console.WriteLine("Data add successful");
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+         
             }
         }
 
--- EmployeePayRollService/EmployeePayRollService/Program.cs
+++ EmployeePayRollService/EmployeePayRollService/Program.cs
@@ -7,7 +7,26 @@ namespace EmployeePayRollService
         static void Main(string[] args)
         {
             Console.WriteLine("****Welcome to Employee PayRoll****");
+            //UC2
             EmployeeRepo employeeRepo = new EmployeeRepo();
+            employeeRepo.GetAllEmployee();
+            Console.WriteLine();
+            //UC4:-Update Salary 
+            EmployeePayroll employee = new EmployeePayroll();
+            employee.EmployeeName = "Tom";
+            employee.Salary = 40000000;
+            employee.StartDate = Convert.ToDateTime("12 / 31 / 2020");
+            employee.Gender = "Female";
+            employee.PhoneNumber = "+91-7060325698";
+            employee.Department = "Support";
+            employee.Address = "Mumbai";
+            employee.BasicPay = 300000;
+            employee.Deduction = 1000;
+            employee.TaxablePay = 2000;
+            employee.IncomeTax = 1000;
+            employee.NetPay = 1000;
+            employeeRepo.AddEmployee(employee);
+            Console.WriteLine();
             employeeRepo.GetAllEmployee();
         }
     }
