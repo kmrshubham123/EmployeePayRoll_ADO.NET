6d4ec8236660cc6dc61b3117555c330e624128ee
kmrshubham123 <kmrshubham123@gmail.com>
2021-07-11 20:41:24 +0530

    [Kumar Shubham]Added:Data base Store Procedure

---
+++ dbo.SpAddEmployeeDetail.sql
@@ -0,0 +1,22 @@
+﻿create procedure  SpAddEmployeeDetail
+(
+@EmployeeName varchar(32),
+@Salary float,
+@StartDate date,
+@Gender Varchar(32),
+@PhoneNumber varchar(20),
+@Department varchar(30),
+@Address varchar(30),
+@BasicPay float,
+@Deduction float,
+@TaxablePay float,
+@IncomeTax float,
+@NetPay float
+
+)
+ as
+ begin
+	INSERT INTO EmployeePayroll (EmployeeName,Salary,StartDate,Gender,PhoneNumber,Department,Address,BasicPay,Deduction,TaxablePay,IncomeTax,NetPay) 
+VALUES (@EmployeeName,@Salary,@StartDate,@Gender,@PhoneNumber,@Department,@Address,
+	@BasicPay,@Deduction,@TaxablePay,@IncomeTax,@NetPay)
+ End
